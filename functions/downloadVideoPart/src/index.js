983dfae0e7fb13548f22d7931e1661aed7fe257b
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-06-03 20:46:55 +0200

    :sparkles: finish basic video part download

--- functions/downloadVideoPart/src/index.js
+++ functions/downloadVideoPart/src/index.js
@@ -1,9 +1,25 @@
 const sdk = require("node-appwrite");
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const crypto = require("crypto");
 
 module.exports = async function (req, res) {
   let logString = "";
   function log(param) {
     logString += param + " \n";
+  }
+
+  async function downloadVideo(youtubeVideoId, filename) {
+    await new Promise((resolve, reject) => {
+      const stream = ytdl(`https://www.youtube.com/watch?v=${youtubeVideoId}`).pipe(
+        fs.createWriteStream(filename)
+      );
+
+      stream.on("finish", () => {
+        log("downloaded video");
+        resolve();
+      });
+    });
   }
 
   const client = new sdk.Client();
@@ -27,12 +43,18 @@ module.exports = async function (req, res) {
     .setSelfSigned(true);
 
   const data = JSON.parse(req.env["APPWRITE_FUNCTION_EVENT_DATA"]);
-  const { youtubeId, start, end } = data;
+  const { youtubeVideoId, start, end } = data;
 
-  log("success");
+  const filename = crypto.randomUUID();
+
+  await downloadVideo(youtubeVideoId, filename);
+
+  await storage.createFile("video_parts", "unique()", filename);
+
+  log(`video ${filename} created`);
 
   res.json({
     log: logString,
-    data
+    data,
   });
 };
