b482de3f8c5f271ff44c66db5dd6b55bc46b1034
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-06-04 18:40:03 +0200

    :sparkles: add oauth and logout buttons

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,30 +1,41 @@
 import { AppwriteContext } from "@/contexts/AppwriteContext";
+import { Button } from "@mui/material";
 import { AppProps } from "next/app";
 import { useContext, useEffect, useState } from "react";
 
 export default function TrainingApp({ Component, pageProps }: AppProps) {
-    const appwrite = useContext(AppwriteContext);
+  const appwrite = useContext(AppwriteContext);
 
-    const [sessionCreated, setSessionCreated] = useState<boolean>(false);
+  const [loggedIn, setLoggedIn] = useState<boolean | undefined>(undefined);
 
-    useEffect(() => {
-        async function createSession() {
-            let session;
-            try {
-                session = await appwrite.account.get()
-            } catch (error) {
+  async function oAuthLogin(provider: "discord" | "github") {
+    appwrite.account.createOAuth2Session(
+      provider,
+      `${window.location.href}`,
+      `${window.location.href}#error`
+    );
+  }
 
-            }
-            if (!session) {
-                await appwrite.account.createAnonymousSession();
-            }
-            setSessionCreated(true)
-        }
-        createSession();
-    }, [appwrite.account])
-
-    if (!sessionCreated) {
-        return <>creating session...</>
+  useEffect(() => {
+    async function checkLoggedIn() {
+      let session;
+      try {
+        session = await appwrite.account.get();
+      } catch (error) {}
+      setLoggedIn(session ? true : false);
     }
-    return <Component {...pageProps} />
+    checkLoggedIn();
+  }, [appwrite.account]);
+
+  if (loggedIn === undefined) {
+    return <>checkLoggedIn...</>;
+  } else if (!loggedIn) {
+    return (
+      <>
+        <Button onClick={() => oAuthLogin("discord")}>Discord Login</Button>
+        <Button onClick={() => oAuthLogin("github")}>GitHub Login</Button>
+      </>
+    );
+  }
+  return <Component {...pageProps} />;
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,26 @@
+import { AppwriteContext } from "@/contexts/AppwriteContext";
+import { Button } from "@mui/material";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+  const appwrite = useContext(AppwriteContext);
+
+  const router = useRouter();
+
+  return (
+    <div>
+      Welcome to Next.js!
+      <Button
+        onClick={async () => {
+          await appwrite.account.deleteSessions();
+          router.reload();
+        }}
+      >
+        Logout
+      </Button>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
