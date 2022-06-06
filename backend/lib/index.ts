4150225cebf533c7d4167738b98a3e8f19a58e30
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-06-06 17:42:10 +0200

    :sparkles: download youtube video

--- backend/lib/index.ts
+++ backend/lib/index.ts
@@ -1,4 +1,6 @@
 import * as admin from "firebase-admin";
+import { createWriteStream } from "fs";
+import ytdl from "ytdl-core";
 
 var serviceAccount = require("../serviceAccountKey.json");
 
@@ -10,10 +12,33 @@ const firestore = admin.firestore();
 
 const videoPartsCollection = firestore.collection("VideoParts");
 
+async function downloadVideo(
+  youtubeVideoId: string,
+  filename: string,
+  start: number,
+  end: number
+) {
+  await new Promise((resolve, reject) => {
+    const stream = ytdl(`https://www.youtube.com/watch?v=${youtubeVideoId}`, {
+      //range: { start: secondToByte(start), end: secondToByte(end) },
+      begin: start,
+      quality: "136",
+    }).pipe(createWriteStream(filename));
+
+    stream.on("finish", () => {
+      console.log("downloaded video");
+      resolve("downloaded video");
+    });
+  });
+}
+
 async function main() {
   const docs = await videoPartsCollection.listDocuments();
   const videoParts = await Promise.all(docs.map((doc) => doc.get()));
   videoParts.forEach((v) => console.log(v.data()));
+
+  const videoPart = videoParts[0].data();
+  await downloadVideo(videoPart!.youtubeVideoId, `temp/${videoPart!.youtubeVideoId}.mp4`, videoPart!.start, videoPart!.end);
 }
 
 main();
