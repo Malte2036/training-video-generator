5247f21bcdf56866a14f8f7b68857cffa54b066d
Malte2036 <18309412+Malte2036@users.noreply.github.com>
2022-06-06 17:31:17 +0200

    :sparkles: fetch VideoParts from firestore

--- backend/lib/index.ts
+++ backend/lib/index.ts
@@ -1,1 +1,19 @@
-console.log("backend starting...")
+import * as admin from "firebase-admin";
+
+var serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const firestore = admin.firestore();
+
+const videoPartsCollection = firestore.collection("VideoParts");
+
+async function main() {
+  const docs = await videoPartsCollection.listDocuments();
+  const videoParts = await Promise.all(docs.map((doc) => doc.get()));
+  videoParts.forEach((v) => console.log(v.data()));
+}
+
+main();
